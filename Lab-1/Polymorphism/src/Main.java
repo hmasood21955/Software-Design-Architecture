34b511c0fbc0b61d75d1cf33f43e31b9d88cdc91
Muhammad Masood Hussain <144346171+hmasood21955@users.noreply.github.com>
2025-03-12 07:17:47 -0800

    Car moves on roads

--- Lab-1/Polymorphism/src/Main.java
+++ Lab-1/Polymorphism/src/Main.java
@@ -3,3 +3,10 @@ class Vehicle {
         System.out.println("This vehicle is moving.");
     }
 }
+
+class Car extends Vehicle {
+    @Override
+    public void move() {
+        System.out.println("Car is driving on the road.");
+    }
+}
