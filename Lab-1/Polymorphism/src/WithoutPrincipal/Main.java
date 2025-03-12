2bb29832e99e10dfa00aac4cb5dec0c52992026c
Muhammad Masood Hussain <144346171+hmasood21955@users.noreply.github.com>
2025-03-12 08:07:17 -0800

    WithoutPrincipal

---
+++ Lab-1/Polymorphism/src/WithoutPrincipal/Main.java
@@ -0,0 +1,31 @@
+package WithoutPrincipal;
+class Car {
+    public void move() {
+        System.out.println("Car is driving on the road.");
+    }
+}
+
+class Bike {
+    public void move() {
+        System.out.println("Bike is pedaling on the street.");
+    }
+}
+
+class Plane {
+    public void move() {
+        System.out.println("Plane is flying in the sky.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        Bike myBike = new Bike();
+        Plane myPlane = new Plane();
+
+        // Calling move() for each vehicle individually
+        myCar.move();   // Output: Car is driving on the road.
+        myBike.move();  // Output: Bike is pedaling on the street.
+        myPlane.move(); // Output: Plane is flying in the sky.
+    }
+}
