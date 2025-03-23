5e7a252177f355b4416cd73d52d097cfcf30a50b
Muhammad Masood Hussain <144346171+hmasood21955@users.noreply.github.com>
2025-03-23 05:47:30 -0800

    SMSSupportListener

--- Lab-3/Design-Patterns/src/observer/eventsmanagement/SMSSupportListener.java
+++ Lab-3/Design-Patterns/src/observer/eventsmanagement/SMSSupportListener.java
@@ -2,3 +2,11 @@ package observer.eventsmanagement;
 
 import java.io.File;
 
+public class SMSSupportListener implements EventListener {
+    private String phoneNumber;
+    private static final int MAX_SMS_LENGTH = 160;
+
+    public SMSSupportListener(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
