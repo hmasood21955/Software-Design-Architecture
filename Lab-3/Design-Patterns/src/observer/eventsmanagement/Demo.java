6d97de09296ae409b28c0ca62143a230cf9f561a
Muhammad Masood Hussain <144346171+hmasood21955@users.noreply.github.com>
2025-03-23 03:51:02 -0800

    create new file

--- Lab-3/Design-Patterns/src/observer/eventsmanagement/Demo.java
+++ Lab-3/Design-Patterns/src/observer/eventsmanagement/Demo.java
@@ -2,9 +2,15 @@ package observer.eventsmanagement;
 
 public class Demo {
     public static void main(String[] args) {
+        System.out.println("Creating a new file editor...");
         Editor editor = new Editor();
         editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
         editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
+    
+        System.out.println("Setting up notifications...");
+        editor.events.subscribe("open", new LogOpenListener("file_log.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("user@example.com"));
+        editor.events.subscribe("save", new SMSSupportListener("+1234567890"));
 
         try {
             editor.openFile("test.txt");
