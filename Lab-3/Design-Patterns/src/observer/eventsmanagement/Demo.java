f64cf984fdbe8c1b80ad04074d1c4535f1da8010
Muhammad Masood Hussain <144346171+hmasood21955@users.noreply.github.com>
2025-03-23 03:56:28 -0800

    Update Demo.java

--- Lab-3/Design-Patterns/src/observer/eventsmanagement/Demo.java
+++ Lab-3/Design-Patterns/src/observer/eventsmanagement/Demo.java
@@ -4,8 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Creating a new file editor...");
         Editor editor = new Editor();
-        editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
-        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
     
         System.out.println("Setting up notifications...");
         editor.events.subscribe("open", new LogOpenListener("file_log.txt"));
@@ -13,7 +11,6 @@ public class Demo {
         editor.events.subscribe("save", new SMSSupportListener("+1234567890"));
 
         try {
-            editor.openFile("test.txt");
             System.out.println("\n--- Testing file open ---");
             editor.openFile("test_document.txt");
             
@@ -21,7 +18,6 @@ public class Demo {
             editor.saveFile();
             
         } catch (Exception e) {
-            e.printStackTrace();
             System.out.println("Oops! Something went wrong: " + e.getMessage());
         }
     }
