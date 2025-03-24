e39dc3acd822ef3c78296bf457ab25b5b713ced9
Muhammad Masood Hussain <144346171+hmasood21955@users.noreply.github.com>
2025-03-24 03:34:54 -0800

    ADD

--- Lab-3/Observer/src/EventManager/Demo.java
+++ Lab-3/Observer/src/EventManager/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
         Editor editor = new Editor();
         editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
         editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
-
+        editor.events.subscribe("save", new SMSSupportListener("+1234567890"));
         try {
             editor.openFile("test.txt");
             editor.saveFile();
